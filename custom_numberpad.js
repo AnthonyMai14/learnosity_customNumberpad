9f02d2ab7cba4a1704fb328fcd81e8bba8af1c7f
Anthony Mai <anthonymai14@gmail.com>
2021-05-08 01:29:01 -0700

    Upload initial assignment

---
+++ custom_numberpad.js
@@ -0,0 +1,146 @@
+/*global LearnosityAmd*/
+LearnosityAmd.define(['jquery-v1.10.2'], function ($) {
+    'use strict';
+    //function to build up HTML table and add onclick handlers
+    function buildHtmlAndInteractions(init, lrnUtils) {
+
+        var $htmlObj;
+        //
+        //Learnosity Tech Test: Complete here
+        //
+        //Build up the structure you want to use here, and add any event handling you may want to use
+        //Set structure for number pad and output
+        $htmlObj = $('<div class="grid-container"><output id="question_input" type="text"><div class="inner-grid-container"><div class="output" id="firstVal"></div><div class="output" id="secondVal"></div><div class="output" id="thirdVal"></div></div></output></output><button class="col col1" value="0">0</button><button class="col col2" value="0">0</button><button class="col col3" value="0">0</button><button class="col col1" value="1">1</button><button class="col col2" value="1">1</button><button class="col col3" value="1">1</button><button class="col col1" value="2">2</button><button class="col col2" value="2">2</button><button class="col col3" value="2">2</button><button class="col col1" value="3">3</button><button class="col col2" value="3">3</button><button class="col col3" value="3">3</button><button class="col col1" value="4">4</button><button class="col col2" value="4">4</button><button class="col col3" value="4">4</button><button class="col col1" value="5">5</button><button class="col col2" value="5">5</button><button class="col col3" value="5">5</button><button class="col col1" value="6">6</button><button class="col col2" value="6">6</button><button class="col col3" value="6">6</button><button class="col col1" value="7">7</button><button class="col col2" value="7">7</button><button class="col col3" value="7">7</button><button class="col col1" value="8">8</button><button class="col col2" value="8">8</button><button class="col col3" value="8">8</button><button class="col col1" value="9">9</button><button class="col col2" value="9">9</button><button class="col col3" value="9">9</button></div></div>');
+        
+        //return either as JQuery Element or String of HTML
+        return $htmlObj;
+    }
+
+    //function to change UI based on correct or incorrect answer status
+    function addValidationUI(questionAnswerStatus) {
+        //
+        //Learnosity Tech Test: Set solution to indicate whether solution is valid or invalid
+        //
+        var inputField = $('#question_input');
+        inputField.removeClass('invalid valid');
+        if (questionAnswerStatus == false) {
+            inputField.addClass('invalid');
+        } else {
+            inputField.addClass('valid');
+        }
+       
+    }
+
+    //Learnosity:
+    //Demo framework for custom question starts here. 
+    //You don't have to change anything, except for the "isValid() function and the DOM element (#question_input) storing response
+    //but you can if you see something useful.
+
+    function CustomNumberPad(init, lrnUtils) {
+
+        //create example table and button elements for constructing numberpad.
+        var $questionTypeHtml = buildHtmlAndInteractions(init);
+        this.$el = init.$el;
+        
+        // add Check Answer button
+        init.$el.html($questionTypeHtml);
+        init.$el.append('<div data-lrn-component="checkAnswer"/>');
+        lrnUtils.renderComponent('CheckAnswerButton', this.$el.find('[data-lrn-component="checkAnswer"]').get(0));
+        
+        //Sense trigger from Check Answer button
+        this.$el.find('[data-lrn-component="checkAnswer"]').click(function () { 
+            init.events.trigger('changed', $(this).val()); 
+        });
+
+        //add on validate
+        init.events.on('validate', function () {
+            init.response = $('#question_input').val();
+            // Create scorer
+            var scorer = new CustomNumberPadScorer(init.question, init.response);
+            //check if answer is correct, and pass true or false to the function to update validation UI
+            addValidationUI(scorer.isValid());
+        });
+
+        //set val to empty for all variables
+        var $valOutput, $firstVal= "", $secondVal= "", $thirdVal = "";
+
+        //Listen to when button is trigger and push value of button into corresponding column
+        $('button').click(function() {
+            //Remove indicaiton of whether soluation invalid/valid because user is changing answer
+            $('#question_input').removeClass('invalid valid');
+            //indentify which column user is inputing from, update appear of button, and grab value
+            if($(this).hasClass('col1')) {
+                //grab value from first col
+                $firstVal = $(this).attr('value');
+                //remove 'clicked' class that hold selected attr from all button in col1
+                $('.col1').removeClass('clicked');
+                //set class to the button to update attribute found in custom_numberpad.css
+                $(this).addClass('clicked');
+            }
+            else if($(this).hasClass('col2')) {
+                //grab value from second col
+                $secondVal = $(this).attr('value');
+                //remove 'clicked' class that hold selected attr from all button in col2
+                $('.col2').removeClass('clicked');
+                //set class to the button to update attribute found in custom_numberpad.css
+                $(this).addClass('clicked');
+            }
+            else if($(this).hasClass('col3')) {
+                //grab value from third col
+                $thirdVal = $(this).attr('value');
+                //remove 'clicked' class that hold selected attr from all button in col3
+                $('.col3').removeClass('clicked');
+                //set class to the button to update attribute found in custom_numberpad.css
+                $(this).addClass('clicked');
+            }
+
+            //update the divs to output value of button just pressed to corresponding column
+            $('#firstVal').html($firstVal);
+            $('#secondVal').html($secondVal);
+            $('#thirdVal').html($thirdVal);
+            
+            //combine the values to be stored into #question_input value
+            $valOutput= $firstVal + $secondVal + $thirdVal;
+            $('#question_input').attr('value', $valOutput);
+        });
+
+        //tell "host API" that this question is ready
+        init.events.trigger('ready');
+    }
+
+    //set question and response
+    function CustomNumberPadScorer(question, response) {
+        this.question = question;
+        this.response = response;
+    }
+
+    //check if answer is valid
+    CustomNumberPadScorer.prototype.isValid = function () {
+        //
+        //Learnosity Tech Test: Complete here
+        //
+        return $('#question_input').val() === this.question.valid_response;
+    };
+    
+    //score
+    CustomNumberPadScorer.prototype.score = function () {
+        return this.isValid() ? this.maxScore() : 0;
+    };
+
+    //max score
+    CustomNumberPadScorer.prototype.maxScore = function () {
+        return this.question.score || 1;
+    };
+
+    //check if a valid response was set so validation can proceed
+    CustomNumberPadScorer.prototype.canValidateResponse = function () {
+        return !!this.question.valid_response;
+    };
+
+    //return custom question and scoring hook to "host API"
+    return {
+        Question: CustomNumberPad,
+        Scorer: CustomNumberPadScorer
+    };
+});
+
